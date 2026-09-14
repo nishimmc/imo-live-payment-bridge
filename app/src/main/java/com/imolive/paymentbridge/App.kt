3a87197ca1f53cca2355f0f86bf30d37e61d320e
nishimmc <162345845+nishimmc@users.noreply.github.com>
2026-09-15 04:45:41 +0600

    Delete app directory

--- app/src/main/java/com/imolive/paymentbridge/App.kt
+++
@@ -1,8 +0,0 @@
-package com.imolive.paymentbridge
-
-import android.app.Application
-
-class App: Application() {
-    override fun onCreate() { super.onCreate(); ctx = this }
-    companion object { lateinit var ctx: App }
-}
